df8568f12bb3fa12836a7c196cb250cff3b8e2c5
Gabriel Aguirre <gaboaguirre25@gmail.com>
2022-10-04 20:24:16 -0500

    route-home

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
+
+
+const homeRoutes = require('./routes/homeRoutes.js');
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
 app.listen(3000, () => (
     console.log('Servidor lanzado en puerto 3000')
 ));
 
-app.get('/', (req, res) => (
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
-));
+app.use('/', homeRoutes); 
+
 app.get('/login', (req, res) => (
     res.sendFile(path.resolve(__dirname, './views/login/login.html'))
 ));
