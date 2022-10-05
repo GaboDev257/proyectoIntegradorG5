2b9bf7ca3bdc218ac59167c72f53f6036698b2eb
JuanOrdoñez <jignacioordonez@gmail.com>
2022-10-04 22:32:51 -0300

    enrutando login

--- src/controllers/usuariosControllers.js
+++ src/controllers/usuariosControllers.js
@@ -3,7 +3,11 @@ const path = require('path');
 const controlador = {
     register: (req,res) => {
         res.sendFile(path.resolve(__dirname, '../views/register/register.html'))
+    },
+    login: (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/login/login.html'))
     }
+    
 }
 
 module.exports = controlador
