4b9a32def2b9d16091ace487c0fb803f3d5d8c54
caminayme <camilanayme@gmail.com>
2022-10-04 22:38:16 -0300

    carrito-parte1

--- src/app.js
+++ src/app.js
@@ -1,29 +1,37 @@
+const carritoRoutes = require ('./routes/carritoRoutes');
+
 const express = require('express');
+const { dirname } = require('path');
 const path = require('path');
 
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+
+app.use('/shoppingCart', carritoRoutes);
+ 
+
+app.use('/', (req, res) => (
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+));
+
+
+app.use('/login', (req, res) => (
+    res.sendFile(path.resolve(__dirname, './views/login/login.html'))
+));
+app.use('/register', (req, res) => (
+    res.sendFile(path.resolve(__dirname, './views/register/register.html'))
+));
+
+
+
+app.use('/ProductDescription', (req, res) => (
+    res.sendFile(path.resolve(__dirname, './views/ProductDescription/productDescription.html'))
+));
 
 app.listen(3000, () => (
     console.log('Servidor lanzado en puerto 3000')
 ));
-
-app.get('/', (req, res) => (
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
-));
-app.get('/login', (req, res) => (
-    res.sendFile(path.resolve(__dirname, './views/login/login.html'))
-));
-app.get('/register', (req, res) => (
-    res.sendFile(path.resolve(__dirname, './views/register/register.html'))
-));
-app.get('/shoppingCart', (req, res) => (
-    res.sendFile(path.resolve(__dirname, './views/shoppingCart/shoppingCart.html'))
-));
-app.get('/ProductDescription', (req, res) => (
-    res.sendFile(path.resolve(__dirname, './views/ProductDescription/productDescription.html'))
-));
-
-
