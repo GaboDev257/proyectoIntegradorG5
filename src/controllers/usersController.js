b78a05c63bc9b12e7d4a6f9761757660720ecc11
Gabriel Aguirre <gaboaguirre25@gmail.com>
2022-11-17 22:57:55 -0500

    user-CRUD

---
+++ src/controllers/usersController.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { validationResult } = require('express-validator')
+
+const productsFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+const controlador = {
+	// LOGIN USER
+	login: (req, res) => {
+        res.render('./users/login.ejs')
+    },
+
+	// DETAIL PROFILE
+    profile: (req, res) => {
+        res.render('./users/profile.ejs')
+    },
+
+    // Form to create
+    register: (req,res) => {
+        res.render( './users/register')
+	},
+
+    // Create -  Method to store
+    store: (req, res) => {
+
+		let errors = validationResult(req);
+		console.log("errors ", errors)
+
+		if ( errors.isEmpty() ) {
+
+			idNuevo=0;
+
+		for (let s of users){
+			if (idNuevo<s.id){
+				idNuevo=s.id;
+			}
+		}
+
+		idNuevo++;
+
+		let nombreImagen = req.file.filename;
+
+
+		let usuarioNuevo =  {
+			id: 1,
+            name: "Juan",
+            lastName: "Maldonado",
+            userName: "juancito",
+            email: "jcmaldonado@gmail.com",
+            birthDate: "20/06/1990",
+			image: nombreImagen
+		};
+
+		users.push(usuarioNuevo);
+
+		fs.writeFileSync(usersFilePath, JSON.stringify(users,null,' '));
+
+		res.redirect('/');
+
+		
+		}
+		else{
+			res.render('products/newProductForm', {errors: errors.array() } ); 
+		}
+	
+		
+	},
+    
+
+    
+}
+
+module.exports = controlador;
--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -1,11 +1,32 @@
-const usuariosControllers = require('./../controllers/usuariosControllers')
+const usersController = require('../controllers/usersController')
 
 const express = require ('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const { check } = require('express-validator');
 
-router.get('/register', usuariosControllers.register)
-router.get('/login', usuariosControllers.login)
-router.get('/profile', usuariosControllers.profile)
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/img/users'));    // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName = 'img' + '-' + file.originalname;   // milisegundos y extensión de archivo original
+     cb(null, imageName);         
+    }
+}); 
+
+
+/*** LOGIN USER ***/
+router.get('/login', usersController.login)
+
+/*** CREATE ONE USER ***/ 
+router.get('/register', usersController.register) 
+router.post('/register', uploadFile.single('imagenUsuario'), usuariosController.store); 
+
+/*** EDIT ONE PROFILE ***/ 
+router.get('/profile/:id', usersController.profile); 
+router.put('/profile/:id', usersController.update); 
 
 
 module.exports = router 
