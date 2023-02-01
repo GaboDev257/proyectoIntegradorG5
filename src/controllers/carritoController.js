5937c9c854c6f0ca18462f4e27a5bf818ef72a18
caminayme <camilanayme@gmail.com>
2023-02-01 19:31:45 -0300

    cambios

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -5,6 +5,10 @@ const controlador = {
   carrito:  (req,res)  => {
     res.render( './shoppingCart.ejs')
   },
+  carritoProducto: async (req, res) => {
+    let prodCarrito = await apiServices.productByPk(req);
+    res.json(prodCarrito);
+  }
 
 }
 
